15e8d8925e0e726a3b19b0243315a5ca2119efcd
EdgarsMielavs <elavdesign@gmail.com>
2017-07-03 16:55:21 +0300

    Created selectedBookController

---
+++ Bootcamp/src/main/java/com/bootcamp/SelectedBookController.java
@@ -0,0 +1,43 @@
+package com.bootcamp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SelectedBookController {
+
+	@Autowired
+	BooksRepository bookrepository;
+	
+	@RequestMapping(value={"/selectedBook"}, method = RequestMethod.GET)
+	public ModelAndView selectedBook(@RequestParam(value="title")String title){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("book",bookrepository.findFirstByTitleIgnoreCase(title));
+		modelAndView.setViewName("NewFile"); //demo or JAVA
+		return modelAndView;
+	}
+	
+	
+	
+	/*@RequestMapping(value={"/selectedBook"}, method = RequestMethod.POST)
+	public ModelAndView SendRequest(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("book",book);
+		//model.addAttribute("author", author);
+		//model.addAttribute("year", year);
+		//model.addAttribute("isbn", isbn);
+		modelAndView.setViewName("test2");
+		return modelAndView;
+	}
+	*/
+}
