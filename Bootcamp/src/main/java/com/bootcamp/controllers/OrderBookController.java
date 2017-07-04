4998c00a6e18e81f6526f061fa3f098dd858196c
Dainis Plasis <dainis.plasis@gmail.com>
2017-07-04 14:48:55 +0300

    some modifications for the demo

--- Bootcamp/src/main/java/com/bootcamp/controllers/OrderBookController.java
+++ Bootcamp/src/main/java/com/bootcamp/controllers/OrderBookController.java
@@ -19,7 +19,7 @@ public class OrderBookController {
 	@Autowired
 	private BooksRepository br;
 	
-	@RequestMapping(value={"/orderbook"}, method = RequestMethod.GET) // if request method is GET then will do this
+	@RequestMapping(value={"/orderBook"}, method = RequestMethod.GET) // if request method is GET then will do this
 	public @ResponseBody ModelAndView searchBook() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("orderBook");  // shows orderBook.html page
@@ -27,11 +27,11 @@ public class OrderBookController {
 		return mav; 
 	}
 	
-	@RequestMapping(value={"/orderbook"}, method = RequestMethod.POST)  // if request method is POST then will do this
+	/*@RequestMapping(value={"/orderBook"}, method = RequestMethod.POST)  // if request method is POST then will do this
 	public @ResponseBody List<Books> orderBook(@RequestParam(value="title", required = false) String title,@RequestParam(value="author",required = false) String author,
 			@RequestParam(value="rating",required = false) Float rating,@RequestParam(value ="isbn",required =  false) String isbn) {
 
 		return br.getBooksByInfo(title, author, rating, isbn); // RETURNS BOOK list
-	}
+	}*/
 
 }
